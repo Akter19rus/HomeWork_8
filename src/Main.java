e0c9d4af8dd894e6c46a73f1e568a0c99a2de457
Владислав <akter.res.hak@gmail.com>
2023-08-02 02:09:55 +0700

    Проверьте ДЗ №8

--- src/Main.java
+++ src/Main.java
@@ -1,5 +1,114 @@
+import java.util.Arrays;
 public class Main {
     public static void main(String[] args) {
-        System.out.println("Hello world!");
+        task1();
+        task2();
+        task3();
+        task4();
+    }
+
+    public static void task1() {
+        System.out.println("            Задача 1");
+        System.out.println("Целочисленный массив");
+        int[] oneMassive = new int[3];
+        oneMassive[0] = 1;
+        oneMassive[1] = 2;
+        oneMassive[2] = 3;
+        for (int i : oneMassive) {
+            System.out.println(i);
+        }
+        System.out.println("Дробный массив");
+        double[] twoMassive = {1.57, 7.654, 9.986};
+        for (double j : twoMassive) {
+            System.out.println(j);
+        }
+        System.out.println("Произвольный массив");
+        int[] threeMassive = new int[6];
+        for (int index = 0; index < threeMassive.length; index++) {
+            threeMassive[index] = index * 4;
+            System.out.println(threeMassive[index]);
+        }
+    }
+
+    public static void task2() {
+        System.out.println("            Задача 2");
+        System.out.println("Целочисленный массив");
+        int[] oneMassive = new int[3];
+        oneMassive[0] = 1;
+        oneMassive[1] = 2;
+        oneMassive[2] = 3;
+        for (int index = 0; index < oneMassive.length; index++) {
+            if (index == oneMassive.length - 1) {
+                System.out.println(oneMassive[index]);
+                break;
+            }
+            System.out.print(oneMassive[index] + ", ");
+        }
+        System.out.println("Дробный массив");
+        double[] twoMassive = {1.57, 7.654, 9.986};
+        for (int index1 = 0; index1 < twoMassive.length; index1++) {
+            if (index1 == twoMassive.length - 1) {
+                System.out.println(twoMassive[index1]);
+                break;
+            }
+            System.out.print(twoMassive[index1] + ", ");
+        }
+        System.out.println("Произвольный массив");
+        int[] threeMassive = new int[6];
+        for (int index2 = 0; index2 < threeMassive.length; index2++) {
+            threeMassive[index2] = index2 * 4;
+            if (index2 == threeMassive.length - 1) {
+                System.out.println(threeMassive[index2]);
+                break;
+            }
+            System.out.print(threeMassive[index2] + ", ");
+        }
+    }
+
+    public static void task3() {
+        System.out.println("            Задача 3");
+        System.out.println("Целочисленный массив");
+        int[] oneMassive = new int[3];
+        oneMassive[0] = 1;
+        oneMassive[1] = 2;
+        oneMassive[2] = 3;
+        for (int index = oneMassive.length - 1; index >= 0; index--) {
+            if (index == 0) {
+                System.out.println(oneMassive[index]);
+                break;
+            }
+            System.out.print(oneMassive[index] + ", ");
+        }
+        System.out.println("Дробный массив");
+        double[] twoMassive = {1.57, 7.654, 9.986};
+        for (int index1 = twoMassive.length - 1; index1 >= 0; index1--) {
+            if (index1 == 0) {
+                System.out.println(twoMassive[index1]);
+                break;
+            }
+            System.out.print(twoMassive[index1] + ", ");
+        }
+        System.out.println("Произвольный массив");
+        int[] threeMassive = new int[6];
+        for (int index2 = threeMassive.length - 1; index2 >= 0; index2--) {
+            threeMassive[index2] = index2 * 4;
+            if (index2 == 0) {
+                System.out.println(threeMassive[index2]);
+                break;
+            }
+            System.out.print(threeMassive[index2] + ", ");
+        }
+    }
+
+    public static void task4() {
+        System.out.println("            Задача 4");
+        int[] oneMassive = {1, 2, 3};
+
+        for (int index = 0; index < oneMassive.length; index++) {
+            if (oneMassive[index] % 2 != 0) {
+                oneMassive[index] += 1;
+                System.out.println(Arrays.toString(oneMassive));           //Почему через Arrays выводится каждое изменение значения массива, а не конечный результат?
+            }
+        }
     }
 }
